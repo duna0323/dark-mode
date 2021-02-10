2ee14cbcd4a59a956d7fdfc10ef209d83fbd2a5d
zavi3 <duna.blue@gmail.com>
2021-02-11 08:27:04 +0900

    3rd

--- app/entry.js
+++ app/entry.js
@@ -1,16 +1,13 @@
-
-
-(function($) { "use strict";
-		
-	//Page cursors
-
+ "use strict";
+    	
+    //Page cursors
     document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
         t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
+    	t.style.top = n.clientY + "px", 
+    	e.style.left = n.clientX + "px", 
+    	e.style.top = n.clientY + "px", 
+    	i.style.left = n.clientX + "px", 
+    	i.style.top = n.clientY + "px"
     });
     var t = document.getElementById("cursor"),
         e = document.getElementById("cursor2"),
@@ -28,42 +25,39 @@
     function o(t) {
         t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
     }
-	
-	//Navigation
-
-	var app = function () {
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-			body = document.querySelector('body');
-			menu = document.querySelector('.menu-icon');
-			menuItems = document.querySelectorAll('.nav__list-item');
-			applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-			menu.addEventListener('click', function () {
-				return toggleClass(body, 'nav-active');
-			});
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}();
-
-	
-	//Switch light/dark
-	
-	$("#switch").on('click', function () {
-		if ($("body").hasClass("light")) {
-			$("body").removeClass("light");
-			$("#switch").removeClass("switched");
-		}
-		else {
-			$("body").addClass("light");
-			$("#switch").addClass("switched");
-		}
-	});          
+    
+    //Navigation
+    var app = function () {
+    	var body = undefined;
+    	var menu = undefined;
+    	var menuItems = undefined;
+    	var init = function init() {
+    		body = document.querySelector('body');
+    		menu = document.querySelector('.menu-icon');
+    		menuItems = document.querySelectorAll('.nav__list-item');
+    		applyListeners();
+    	};
+    	var applyListeners = function applyListeners() {
+    		menu.addEventListener('click', function () {
+    			return toggleClass(body, 'nav-active');
+    		});
+    	};
+    	var toggleClass = function toggleClass(element, stringClass) {
+    		if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+    	};
+    	init();
+    }();
+    
+    //Switch light/dark
+	import $ from 'jquery';
+    $("#switch").on('click', function () {
+    	if ($("body").hasClass("light")) {
+    		$("body").removeClass("light");
+    		$("#switch").removeClass("switched");
+    	}
+    	else {
+    		$("body").addClass("light");
+    		$("#switch").addClass("switched");
+    	}
+    });          
               
-})(jQuery); 
